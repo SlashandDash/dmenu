39c1e2f2dcbb344ccc2f0934b9dcd7b19a5af62a
SlashandDash <slashinganddashing@gmail.com>
2024-05-23 20:26:43 -0400

    Redeed everything

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=10"
+	"HackNerdFont:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#000000" },
-	[SchemeSel] = { "#eeeeee", "#4287f5" },
+	[SchemeNorm] = { "#bbbbbb", "#111212" },
+	[SchemeSel] = { "#eeeeee", "#57637a" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
